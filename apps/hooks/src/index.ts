4e407da3785ea2878461842a01ad6818f3cf6ae8
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-12-01 18:41:23 +0530

    fix: db import

--- apps/hooks/src/index.ts
+++ apps/hooks/src/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import express from "express";
+import prisma from "@repo/db";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,9 +11,34 @@ app.get("/healthy", (req: Request, res: Response) => {
   });
 });
 
-app.post("/hooks/catch/:userId/:zapId", (req: Request, res: Response) => {
+app.post("/hooks/catch/:userId/:zapId", async (req: Request, res: Response) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
+  const { metadata } = req.body;
+
+  try {
+    await prisma.$transaction(async (tx) => {
+      if (!zapId) {
+        return res.status(400).json({
+          message: "NO zapID",
+        });
+      }
+      const run = await tx.zapRun.create({
+        data: {
+          metadata: metadata,
+          zapId: zapId,
+        },
+      });
+
+      await tx.zapRunOutbox.create({
+        data: {
+          zapRunId: run.id,
+        },
+      });
+    });
+  } catch (error) {
+    console.log("error while database transaction", error);
+  }
 });
 
 app.listen(PORT, () => {
